c3844358cc2dbf4a6b1f2be24198ee75f7c148bf
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-19 19:00:38 +0200

    feat: add mock game data

---
+++ test/mocks/game.js
@@ -0,0 +1,126 @@
+exports.gameProps = {
+  id: 'xwfKQKPk0kK_tpxPAABy',
+  initiator: {
+    id: 'a86cf54e-9c71-44bc-8758-7f4d6dd3af9a',
+    name: 'Janosh',
+    avatar: '0',
+    games: {
+      played: '',
+      won: ''
+    }
+  },
+  playerList: [
+    {
+      user: {
+        id: 'a86cf54e-9c71-44bc-8758-7f4d6dd3af9a',
+        name: 'Janosh',
+        avatar: '2',
+        games: {
+          played: '10',
+          won: '4'
+        }
+      },
+      faction: 'liberal',
+      hitler: false,
+      president: false,
+      chancellor: false,
+      executed: false,
+      chancellorVote: null
+    },
+    {
+      user: {
+        id: '3caea987-09b9-40dc-a592-e6acff3cc9f4',
+        name: 'Oscar',
+        avatar: '0',
+        games: {
+          played: '',
+          won: ''
+        }
+      },
+      faction: 'fascist',
+      hitler: false,
+      president: false,
+      chancellor: false,
+      executed: false,
+      chancellorVote: null
+    },
+    {
+      user: {
+        id: '67a9c920-f52c-4e8b-9327-648eb23e3f38',
+        name: 'Himmler',
+        avatar: '0',
+        games: {
+          played: '',
+          won: ''
+        }
+      },
+      faction: 'liberal',
+      hitler: false,
+      president: false,
+      chancellor: false,
+      executed: false,
+      chancellorVote: null
+    },
+    {
+      user: {
+        id: '0d1e9481-61dd-451e-a88a-ddd2f7a52d65',
+        name: 'Speer',
+        avatar: '1',
+        games: {
+          played: '',
+          won: ''
+        }
+      },
+      faction: 'fascist',
+      hitler: true,
+      president: true,
+      chancellor: false,
+      executed: false,
+      chancellorVote: null
+    },
+    {
+      user: {
+        id: '0b7bbd99-c452-40a2-842e-5c88667c5d45',
+        name: 'Göring',
+        avatar: '3',
+        games: {
+          played: '',
+          won: ''
+        }
+      },
+      faction: 'fascist',
+      hitler: false,
+      president: false,
+      chancellor: false,
+      executed: false,
+      chancellorVote: null
+    }
+  ],
+  message: 'showRoles',
+  numberOfLiberals: 3,
+  numberOfFascists: 2,
+  numberOfLiberalPolicies: 0,
+  numberOfFascistPolicies: 0,
+  acknowledgeCounts: {
+    playerRole: 0,
+    fascists: 0,
+    president: 0,
+    chancellor: 0,
+    chosenPolicy: 0,
+    vetoPower: 0
+  },
+  turnCount: 0,
+  currentPresident: '0d1e9481-61dd-451e-a88a-ddd2f7a52d65',
+  suggestedChancellor: null,
+  chancellorVoteCount: 0,
+  eligiblePolicies: [
+    'fascist',
+    'fascist',
+    'liberal'
+  ],
+  electionFailCount: 0,
+  vetoPowerUnlocked: false,
+  gameOver: false,
+  askPresidentToExecutePlayer: true,
+  executedPlayers: [],
+};
