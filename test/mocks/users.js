551aa8bacc44cea0ca19a6dfbf8b7c161b6afa6c
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-19 19:00:44 +0200

    feat: add mock users data

---
+++ test/mocks/users.js
@@ -0,0 +1,29 @@
+exports.initiator = {
+  name: 'Janosh',
+  id: 'c48ff7b4-9851-4b14-8f39-a53344ebe1b6',
+  avatar: 0,
+},
+
+exports.users = [
+  {
+    name: 'Arol',
+    id: 'a86cf54e-9c71-44bc-8758-7f4d6dd3af9b',
+    avatar: 1,
+  }, {
+    name: 'Oscar',
+    id: '3caea987-09b9-40dc-a592-e6acff3cc9f4',
+    avatar: 2,
+  }, {
+    name: 'Lucie',
+    id: '67a9c920-f52c-4e8b-9327-648eb23e3f38',
+    avatar: 3,
+  }, {
+    name: 'Diego',
+    id: '0d1e9481-61dd-451e-a88a-ddd2f7a52d65',
+    avatar: 4,
+  }, {
+    name: 'Krysh',
+    id: '0b7bbd99-c452-40a2-842e-5c88667c5d45',
+    avatar: 5,
+  },
+];
